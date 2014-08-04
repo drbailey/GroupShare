94ac03ca39fa6d9ed92cacebeec174e92f09f0d8
jzmoore <josiah.moore@gmail.com>
2014-08-04 10:55:02 -0700

    Exercise 11: Asking Questions
    Asking questions w/ raw_input

---
+++ Learning-Python/ex11.py
@@ -0,0 +1,19 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# Additional practice
+
+print "Who is your favorite author?",
+author = raw_input()
+print "What is your favorite book?",
+book = raw_input()
+print "What is your favorite movie?",
+movie = raw_input()
+
+print "So you like %r, you've read %r, and watched %r." % (author, book, movie)
