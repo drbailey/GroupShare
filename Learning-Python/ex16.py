32eb150ad15c609cc274465a4978a467ba7b9a41
jzmoore <josiah.moore@gmail.com>
2014-08-28 18:48:06 -0700

    Exercise 16: Reading and Writing Files

---
+++ Learning-Python/ex16.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+# Opens the file in write mode instead of default read mode.
+print "Opening the file..."
+target = open(filename, 'w')
+
+# Reading the documentation, doesn't seem needed.
+print "Truncating the file. Goodbye!" 
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+# Closes the file
+print "And finally, we close it."
+target.close()
