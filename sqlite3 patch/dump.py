ba011769612993a14e73132f7ec99ec722e6677e
Drew Bailey <Baileyd795@gmail.com>
2015-03-26 11:42:32 -0700

    updated
    removed credit to me since I rolled back my changes to patch.

--- sqlite3 patch/dump.py
+++ sqlite3 patch/dump.py
@@ -17,7 +17,6 @@ def _iterdump(connection):
     """
 
     *** MODIFIED WITH PATCH: http://bugs.python.org/file24429/sqlite3dump.patch ***
-    *** MODIFIED FURTHER BY drew bailey 2015-03-26 ***
 
     Returns an iterator to the dump of the database in an SQL text format.
 
